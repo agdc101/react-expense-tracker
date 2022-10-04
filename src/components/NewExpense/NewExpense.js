df28e2742bbc31fae03cd6a6de94c4be2e25fe85
agdc101 <agdc101@gmail.com>
2022-10-04 09:07:08 +0100

    conditional form added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+    const [formActive, setFormActive] = useState(false); 
+
     const onSaveExpenseData = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.ceil(Math.random() * 1000).toString()
         }
        props.onAddExpense(expenseData);
+       setFormActive(false);
+    }
+
+    const startEditingHandler = () => {
+        setFormActive(true);
+    }
+    const stopEditingHandler = () => {
+        setFormActive(false);
     }
     
     return  (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={onSaveExpenseData} />
+            {formActive === true ? <ExpenseForm stopEditing={stopEditingHandler} onSaveExpenseData={onSaveExpenseData} /> : <button onClick={startEditingHandler}>Add New Expense</button>}
         </div>
     )
 };
